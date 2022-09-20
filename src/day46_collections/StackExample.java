6a0cc94db1fed65f7a6c8f2519d8211024fd55c0
Sevara Yuldasheva <sevara878787@mail.ru>
2022-09-20 11:32:34 -0400

    stack example class created

---
+++ src/day46_collections/StackExample.java
@@ -0,0 +1,23 @@
+package day46_collections;
+
+import java.util.Stack;
+
+public class StackExample {
+
+    public static void main(String[] args) {
+
+        //LIFO --> Last in First out
+        Stack<String> stack = new Stack<>();
+        stack.push("a"); //add
+        stack.push("b");
+        stack.push("c");
+
+        System.out.println(stack);
+        stack.pop(); //removes the last thing that was added
+        System.out.println(stack);
+
+        System.out.println("What is the last thing: "+stack.peek());
+
+
+    }
+}
