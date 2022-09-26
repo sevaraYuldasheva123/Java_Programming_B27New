5bb3fddaad251344c16284057b05488c4edd85a6
Sevara Yuldasheva <sevara878787@mail.ru>
2022-09-26 12:11:23 -0400

    couple of commits

--- src/day46_collections/QueueExamples.java
+++ src/day46_collections/QueueExamples.java
@@ -1,5 +1,6 @@
 package day46_collections;
 
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -17,5 +18,24 @@ public class QueueExamples {
         System.out.println(queue.peek()); //shows what was first
         queue.poll(); // will remove what was first in the prioritized queue
         System.out.println(queue);
+
+        System.out.println("----------------------------");
+
+        ArrayDeque<String> queue2 = new ArrayDeque<>();
+        queue2.offer("java"); // adds elements to thr deque
+        queue2.offer("tuesday");
+        queue2.offer("soft skills");
+        queue2.offer("automation");
+        System.out.println(queue2);
+        System.out.println( queue2.peek()); // sto see what in the beginning
+
+        queue2.poll(); // removes first --> "java" out
+        System.out.println(queue2);
+
+        queue2.offerFirst("API"); // adds new value to the first index
+        queue2.offerLast("Database"); // add value to the last index
+        System.out.println(queue2);
+
+
     }
 }
---
+++ src/day47_collections/IteratorPractice.java
@@ -0,0 +1,32 @@
+package day47_collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorPractice {
+
+    public static void main(String[] args) {
+
+        List <String> names = new ArrayList<>(Arrays.asList("James", "Anna", "Emre", "Jane"));
+
+        for (String each : names){
+            System.out.println(each);
+        }
+
+        System.out.println();
+
+        Iterator<String> it = names.iterator();
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+        System.out.println(it.next());
+
+        Iterator<String> iterator = names.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+    }
+}
